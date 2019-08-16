b36d001c0386dcd3399f78ed8ee98423c2199108
Bartek Szczeciński <bartosz.szczecinski@protonmail.com>
2019-08-16 22:40:21 +0200

    Add saving and retrieving the URL from database

--- urlShortenerApp/models.py
+++ urlShortenerApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class URL(models.Model):
+    url = models.URLField(max_length=300)
+    hashed_url = models.CharField(unique=True, max_length=50)
+
+    def __str__(self):
+        return "URL: " + self.url + "  Hashed: " + self.hashed_url
--- urlShortenerApp/views.py
+++ urlShortenerApp/views.py
@@ -1,18 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+
+from shortuuid import uuid
+
 from . import forms
+from .models import URL
 
 # Create your views here.
 
 def index(request):
     context = {}
+    #If POST then take input from form, and consult the database for a given URL (either return an already exisiting shortened URL from database or create a new one and save to database)
     if request.method == 'POST':
         form = forms.URLform(request.POST)
         context['form'] = form
         if form.is_valid():
-            context['shortened_url'] = form.cleaned_data['urlField']
+            provided_url = form.cleaned_data['urlField']
+            url_in_database = find_url_in_database(provided_url)
+            if(url_in_database is None):
+                shortened_url = uuid(name=provided_url)[:10]
+                new_url_in_database = URL(url=provided_url, hashed_url=shortened_url)
+                new_url_in_database.save()
+            else:
+                shortened_url = url_in_database.hashed_url
+
+            context['shortened_url'] = shortened_url
+    #If GET then create a new, empty form
     else:
         form = forms.URLform()
         context['form'] = form
 
     return render(request, 'urlShortenerApp/index.html', context)
+
+"Return URL model object from database or None if non-existent"
+def find_url_in_database(url):
+    try:
+        url_in_database = URL.objects.get(url=url)
+    except ObjectDoesNotExist:
+        url_in_database = None
+    finally:
+        return url_in_database
